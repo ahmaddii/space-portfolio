48b14dcdde80dd900af729750ba22d2cba4c80cf
ahmaddii <malikahmad6666666@gmail.com>
2025-07-17 17:11:11 +0500

    Refactor HeroContent component for improved readability and maintainability

--- components/sub/hero-content.tsx
+++ components/sub/hero-content.tsx
@@ -23,9 +23,10 @@ export const HeroContent = () => {
           className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]]"
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="Welcome-text text-[13px]">
-            Hello It's Malik Ahmad Rasheed
+           <h1 className="Welcome-text text-[13px]">
+           Hello It&apos;s Malik Ahmad Rasheed
           </h1>
+
         </motion.div>
 
         <motion.div
